4fd3989b2282d301a5f823933575c463f800b261
Jør∂¡ <itsbrickpop@gmail.com>
2024-02-25 14:41:36 +0100

    Minor improvements

---
+++ README.md
@@ -0,0 +1,17 @@
+# AdminJS Deno template
+
+A starter project to build an AdminJS app running on Deno.
+
+## Get started
+
+The current implementation of AdminJS needs a minor workaround:
+
+```sh
+deno task setup
+```
+
+Then, start the server with:
+
+```sh
+deno task dev
+```
--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,3 +1,8 @@
+// Force loading the NPM modules
+import _express from 'npm:express';
+import _AdminJS from 'npm:adminjs';
+import _AdminJsExpress from 'npm:@adminjs/express';
+
 // Run Deno cache
 
 async function setup() {
@@ -6,8 +11,8 @@ async function setup() {
 }
 
 async function setup1() {
-  const targetFile =
-    Deno.cwd() + '/node_modules/.deno/adminjs@7.5.10/node_modules/adminjs/lib/frontend/utils/api-client.js';
+  const prefix = await getLastVersionPath("adminjs")
+  const targetFile = prefix + '/node_modules/adminjs/lib/frontend/utils/api-client.js';
   const cmd = new Deno.Command('sed', {
     args: ['-i', '', '13s/^\}$/}finally{if(!globalAny){globalAny={isOnServer:true};}}/', targetFile],
   });
@@ -17,15 +22,34 @@ async function setup1() {
 }
 
 async function setup2() {
-  const targetFile =
-    Deno.cwd() +
-    '/node_modules/.deno/adminjs@7.5.10/node_modules/adminjs/lib/backend/utils/view-helpers/view-helpers.js';
+  const prefix = await getLastVersionPath("adminjs")
+  const targetFile = prefix + '/node_modules/adminjs/lib/backend/utils/view-helpers/view-helpers.js';
   const cmd = new Deno.Command('sed', {
     args: ['-i', '', '8s/^\}$/}finally{if(!globalAny){globalAny={};}}/', targetFile],
   });
 
   console.log('Patching', targetFile);
   await cmd.output();
+}
+
+async function getLastVersionPath(packageName: string) {
+  const targetPath = Deno.cwd() + '/node_modules/.deno/'
+
+  const items: string[] = []
+  for await(const item of Deno.readDir(targetPath)) {
+    if(!item.name.startsWith(packageName + "@")) continue
+    else if(!item.isDirectory) continue
+
+    items.push(item.name)
+    break
+  }
+  if(!items.length) return null;
+
+  items.sort((a, b) => {
+    if(a < b) return 1;
+    return -1
+  })
+  return targetPath + items[0]
 }
 
 setup();
