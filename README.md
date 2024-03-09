f97c58f6e6a3b0efc51148534c9bd8fd2f262a6c
Jør∂¡ <itsbrickpop@gmail.com>
2024-03-09 23:55:52 +0100

    Updated dependency

--- README.md
+++ README.md
@@ -4,20 +4,18 @@ A starter project to build an AdminJS app running on Deno.
 
 ## Get started
 
-The current implementation of AdminJS needs a minor workaround:
-
-```sh
-deno task setup
-```
-
-Then, define your settings on the `.env` file:
+Define your settings on the `.env` file:
 
 ```sh
 cp .env.example .env
 ```
 
-Then, start the server with:
+Then, start the development server with:
 
 ```sh
+# Optional DB Server
+deno task dev:db &
+
+# Start the app
 deno task dev
 ```
--- scripts/setup.ts
+++ scripts/setup.ts
@@ -6,50 +6,7 @@ import _AdminJsExpress from 'npm:@adminjs/express';
 // Run Deno cache
 
 async function setup() {
-  await setup1();
-  await setup2();
-}
-
-async function setup1() {
-  const prefix = await getLastVersionPath("adminjs")
-  const targetFile = prefix + '/node_modules/adminjs/lib/frontend/utils/api-client.js';
-  const cmd = new Deno.Command('sed', {
-    args: ['-i', '', '13s/^\}$/}finally{if(!globalAny){globalAny={isOnServer:true};}}/', targetFile],
-  });
-
-  console.log('Patching', targetFile);
-  await cmd.output();
-}
-
-async function setup2() {
-  const prefix = await getLastVersionPath("adminjs")
-  const targetFile = prefix + '/node_modules/adminjs/lib/backend/utils/view-helpers/view-helpers.js';
-  const cmd = new Deno.Command('sed', {
-    args: ['-i', '', '8s/^\}$/}finally{if(!globalAny){globalAny={};}}/', targetFile],
-  });
-
-  console.log('Patching', targetFile);
-  await cmd.output();
-}
-
-async function getLastVersionPath(packageName: string) {
-  const targetPath = Deno.cwd() + '/node_modules/.deno/'
-
-  const items: string[] = []
-  for await(const item of Deno.readDir(targetPath)) {
-    if(!item.name.startsWith(packageName + "@")) continue
-    else if(!item.isDirectory) continue
-
-    items.push(item.name)
-    break
-  }
-  if(!items.length) return null;
-
-  items.sort((a, b) => {
-    if(a < b) return 1;
-    return -1
-  })
-  return targetPath + items[0]
+  // Do your set up here
 }
 
 setup();
